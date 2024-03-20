cbbe7ebdb418ef24a9d82144fa31582336d4419d
JohnMutemi <jmtutorsalp@gmail.com>
2024-03-20 13:58:47 +0300

    arrow functions commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function divide(a,b){
+    console.log(`${a}/${b}`);
+    return a/b;
+}
+divide(2000,100);
+
+const square= (x)=>{
+    console.log(`${x}`);
+    return x*x;
+};
+square(2);
+
+
+
+
+const add=(a,b)=>{
+    console.log(`${a}+${b}`);
+    return a+b;
+};
+add(3,4);
